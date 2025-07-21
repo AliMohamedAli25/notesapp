45d8636806037472a666be39915ff7b9a204c493
AliMohamedAli25 <alimohamedali2002.7.8@gmail.com>
2025-07-22 01:26:23 +0300

    add dark theme and create notesview

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:notesapp/views/notes_view.dart';
 
 void main() {
   runApp(const notesapp());
@@ -9,6 +10,13 @@ class notesapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 28, 42, 53),
+      ),
+      home: const NotesView(),
+    );
   }
 }
